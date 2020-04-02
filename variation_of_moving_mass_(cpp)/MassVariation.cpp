31f88639562aae1365018586c83bfb9b0ea8ca74
tasnimzotder <tasnimzotder@gmail.com>
2020-04-02 13:34:28 +0530

    rewrite the MassVariation.cpp file

--- variation_of_moving_mass_(cpp)/MassVariation.cpp
+++ variation_of_moving_mass_(cpp)/MassVariation.cpp
@@ -1,10 +1,25 @@
 #include "MassVariation.h"
+
 #include <cmath>
 
 MassVariation::MassVariation() {
-	this->velocityLight = 3 * pow(10, 8);
+    this->velocityLight = 3 * pow(10, 8);
 }
 
-float MassVariation::getData(float massInit, float velocity) {
-	float denom
+bool MassVariation::getData(float massInit, float velocity) {
+    this->massInit = massInit;
+    this->velocity = velocity;
+
+    float denom = 1.0 - (float)(pow(this->velocity, 2) / pow(this->velocityLight, 2));
+    this->mass = massInit / pow(denom, 0.5);
+
+    if (denom > 0) {
+        return true;
+    } else {
+        false;
+    }
+}
+
+float MassVariation::getMass() {
+    return this->mass;
 }
--- variation_of_moving_mass_(cpp)/MassVariation.h
+++ variation_of_moving_mass_(cpp)/MassVariation.h
@@ -1,14 +1,14 @@
 #pragma once
 class MassVariation {
-protected:
-	float mass;
-	float massInit;
-	float velocity;
-	float velocityLight;
-public:
-	MassVariation();
+   protected:
+    float mass;
+    float massInit;
+    float velocity;
+    float velocityLight;
 
-	void getData(float, float);
-	float getMass();
+   public:
+    MassVariation();
+
+    bool getData(float, float);
+    float getMass();
 };
-
